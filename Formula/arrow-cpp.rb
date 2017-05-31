d57f5fb0f350279159e73bfec143448d32b02656
Kozo Nishida <kozo.nishida@gmail.com>
2017-05-31 12:49:40 +0900

    Update arrow-cpp.rb

--- Formula/arrow-cpp.rb
+++ Formula/arrow-cpp.rb
@@ -22,21 +22,20 @@ class ArrowCpp < Formula
     mkdir build_type.downcase
     chdir build_type.downcase
     system "cmake", "-D", "CMAKE_BUILD_TYPE=#{build_type}", "-D", "CMAKE_INSTALL_PREFIX:PATH=#{prefix}", ".."
-    system "make unittest"
+    system "make", "unittest"
     system "make", "install"
   end
 
   test do
     (testpath/"test.cpp").write <<-EOS.undent
     #include "arrow/api.h"
-
     int main(void)
     {
       arrow::Int64Builder builder(arrow::default_memory_pool(), arrow::int64());
       return 0;
     }
     EOS
-    system ENV.cxx, "test.cpp", "-std=c++11", "-I#{prefix}/include", "-L#{lib}", "-larrow", "-o", "test"
+    system ENV.cxx, "test.cpp", "-std=c++11", "-I#{include}", "-L#{lib}", "-larrow", "-o", "test"
     system "./test"
   end
 end
